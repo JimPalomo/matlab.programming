30d9792f99063f15c89f439c7c84f7fbe10e6b50
- <jim@blackout.hsd1.il.comcast.net>
2021-01-14 14:52:39 -0600

    fixed spacing

--- 8.m
+++ 8.m
@@ -7,10 +7,13 @@ age = 12;
 switch age
     case 0
         disp("Are you even allowed to code?")
+    
     case num2cell(1:18) 
         disp("Still enjoying their childhood")
+    
     case {19 20 21 22 23 24 25}
         disp("Adult life has just begun")
+    
     otherwise
         disp("Good luck")
     end
